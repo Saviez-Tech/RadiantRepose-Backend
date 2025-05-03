08be2f6d642ce5d541cdd0fbea0beb29a91bfb25
unknown <ebubeidika@gmail.com>
2025-05-03 03:20:02 +0100

    admin supabaase run

--- luxury/admin.py
+++ luxury/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import LuxuryBranch,Worker,Product,Transaction,ScannedItem
-# Register your models here.
 
 
+class ProductAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        obj.upload_image_to_supabase()
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(LuxuryBranch)
 admin.site.register(Worker)
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Transaction)
 admin.site.register(ScannedItem)
